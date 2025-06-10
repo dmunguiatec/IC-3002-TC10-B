fe4265e1f59ed2ed3b8a0fcc9a0c65bcd3b797a9
Diego Munguía <dmunguia@itcr.ac.cr>
2025-06-09 18:26:35 -0600

    Arreglando deprecation warning en prueba s

--- simulated_annealing_test.py
+++ simulated_annealing_test.py
@@ -9,7 +9,7 @@ class CasosDePrueba(unittest.TestCase):
     def test_optimizar_peq(self):
         dominio = Dominio3SAT('./datos/f20-91.cnf')
         sol = optimizar(dominio)
-        self.assertEquals(dominio.fcosto(sol), 0)
+        self.assertEqual(dominio.fcosto(sol), 0)
 
     def test_optimizar_gde(self):
         dominio = Dominio3SAT('./datos/f100-430.cnf')
